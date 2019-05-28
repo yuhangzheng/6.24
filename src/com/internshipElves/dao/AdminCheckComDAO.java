64810bd71dd474c4f37a2c2b284ede5e1519c9de
starovo123 <799187444@qq.com>
2019-05-28 15:37:34 +0800

    AdminCheckComDAO

---
+++ src/com/internshipElves/dao/AdminCheckComDAO.java
@@ -0,0 +1,22 @@
+package com.internshipElves.dao;
+
+import com.internshipElves.entity.AdminCheckCom;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("adminCheckComDAO")
+public interface AdminCheckComDAO {
+    int deleteByPrimaryKey(Integer adminCheckComId);
+
+    int insert(AdminCheckCom record);
+
+    int insertSelective(AdminCheckCom record);
+
+    AdminCheckCom selectByPrimaryKey(Integer adminCheckComId);
+
+    int updateByPrimaryKeySelective(AdminCheckCom record);
+
+    int updateByPrimaryKey(AdminCheckCom record);
+
+}
