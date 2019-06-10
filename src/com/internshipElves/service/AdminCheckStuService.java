f3c5a27217b0bfe1e11b016ba7c60365aaaf08e9
azurey0 <923504994@qq.com>
2019-06-10 23:20:57 +0800

    更新了公司端登录，注册，创建工作

---
+++ src/com/internshipElves/service/AdminCheckStuService.java
@@ -0,0 +1,11 @@
+package com.internshipElves.service;
+
+import com.internshipElves.entity.AdminGetStu;
+
+import java.util.List;
+
+public interface AdminCheckStuService {
+    public List<AdminGetStu> queryAll();
+
+    public Integer deleteByStuId(Integer stuId);
+}
